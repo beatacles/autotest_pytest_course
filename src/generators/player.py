deae8a77c787ae181c5bc89ee66f07d77933196d
beatacles <iam.dkalugin@gmail.com>
2023-08-17 13:16:31 +0300

    add updater json any level

--- src/generators/player.py
+++ src/generators/player.py
@@ -21,6 +21,18 @@ class Player:
         self.result['avatar'] = avatar
         return self
 
+    def update_inner_value(self, keys, value):
+        if not isinstance(keys, list):
+            self.result[keys] = value
+        else:
+            temp = self.result
+            for item in keys[:-1]:
+                if  item not in temp.keys():
+                    temp[item] = {}
+                temp = temp[item]
+            temp[keys[-1]] = value
+        return self
+
     def reset(self):
         self.set_status()
         self.set_balance()
--- tests/users/test_06_1_generators.py
+++ tests/users/test_06_1_generators.py
@@ -1,10 +1,20 @@
 """Генерируем тестовые данные для тестов"""
 import pytest
 
+from src.generators.player_localization import Player_localization
+
+
 @pytest.mark.parametrize("status", ["ACTIVE", "BANNED", "DELETED", "INACTIVE"])
-def test_user_generator(status, get_generated_player):
+def test_user_generator_status(status, get_generated_player):
     print(get_generated_player.set_status(status).build())
 
+
 @pytest.mark.parametrize("balance", ["-1", "0", "100", "adsa"])
-def test_user_generator(balance, get_generated_player):
+def test_user_generator_balance(balance, get_generated_player):
     print(get_generated_player.set_balance(balance).build())
+
+
+def test_user_generator_update_data(get_generated_player):
+    object_to_send = get_generated_player.update_inner_value(
+        ['localize', 'en', 'countries'], Player_localization('fr_FR').build()).build()
+    print(object_to_send)
